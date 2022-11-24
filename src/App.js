4fc7044a02b65da66fc671af1d236f79f680b3a0
Karsten Koerner <kgk1999@gmail.com>
2022-11-23 23:33:51 -0500

    Routing ready for page design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/home'
 import Contact from './pages/contact'
 import About from './pages/about'
 import Careers from './pages/careers'
+import './styles/main.css'
 import './styles/home.css'
 import './styles/contact.css'
 import './styles/about.css'
@@ -11,7 +13,15 @@ import './styles/careers.css'
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/> }/>
+        <Route path="home" element={ <Home/> }/>
+        <Route path="about" element={ <About/> }/>
+        <Route path="contact" element={ <Contact/> }/>
+        <Route path="careers" element={ <Careers/> }/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
